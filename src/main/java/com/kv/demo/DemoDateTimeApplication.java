765dfcc09ad70e711ed68dd2b1609c33f5634998
KV <Schumacher4u>
2020-06-10 21:12:41 -0400

    Initial Commit

---
+++ src/main/java/com/kv/demo/DemoDateTimeApplication.java
@@ -0,0 +1,13 @@
+package com.kv.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDateTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoDateTimeApplication.class, args);
+	}
+
+}
