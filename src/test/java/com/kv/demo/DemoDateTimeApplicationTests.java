8fe6b54ef3ec3fb8fd5ba38e4aeedc0b506f7e9b
KV Perumal <indianakv@gmail.com>
2025-01-19 22:26:08 -0500

    Version Update

--- src/test/java/com/kv/demo/DemoDateTimeApplicationTests.java
+++ src/test/java/com/kv/demo/DemoDateTimeApplicationTests.java
@@ -1,10 +1,10 @@
 package com.kv.demo;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import com.kv.demo.controller.AccountDetails;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest(classes = AccountDetails.class)
 class DemoDateTimeApplicationTests {
